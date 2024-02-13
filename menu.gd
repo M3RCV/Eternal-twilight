5621797e6c7d4c63ca8291e1ab2900eaf902cb6e
go1x1loh <go1x1loh@bk.ru>
2024-02-13 22:08:53 +0300

    atyr_pressed_buttons_start_quit
    норм работают кнопки

--- menu.gd
+++ menu.gd
@@ -1,11 +1,19 @@
-extends Node2D
+extends Control
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-	print("надо больше доната123")
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_button_pressed():
+	get_tree().change_scene_to_file("res://level.tscn")
+
+
+func _on_quit_pressed():
+	get_tree().quit()
